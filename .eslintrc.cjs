bf68b5efa576c58850ee41c3a5f320dcc3f2cd93
Skilled5041 <skilled5041@gmail.com>
2024-02-24 12:53:47 -0500

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+	root: true,
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended",
+		"prettier"
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2020,
+		extraFileExtensions: [".svelte"]
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser"
+			}
+		}
+	],
+	rules: {
+		"prefer-const": "warn",
+		"no-var": "error",
+		"no-unused-vars": "warn",
+		"prefer-arrow-callback": "warn",
+		semi: "error",
+		"linebreak-style": ["error", "unix"]
+	}
+};
