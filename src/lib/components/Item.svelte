fb1f74794b973c212cad05fcd904c397e57e34f9
danielp1218 <danielpu2007@gmail.com>
2024-02-24 14:57:57 -0500

    UI init

---
+++ src/lib/components/Item.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import {createEventDispatcher} from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    export let x: number = 0;
+    export let y: number = 0;
+    export let text: string = "Hello World";
+
+    let moving: boolean = false;
+
+    function handleMouseDown() {
+        moving = true;
+    }
+
+
+    function handleMouseMove(e: MouseEvent) {
+        if (moving) {
+            x += e.movementX;
+            y += e.movementY;
+            dispatch('drag', {x, y});
+        }
+    }
+
+    function handleMouseUp() {
+        if (moving) {
+            dispatch('drop', {x, y});
+        }
+        moving = false;
+    }
+</script>
+
+<style>
+    .draggable {
+        user-select: none;
+        cursor: move;
+        position: absolute;
+        pointer-events: auto;
+    }
+
+</style>
+
+<div
+        class="draggable bg-gray-100 border-gray-600 border-4"
+        style="left: {x}px; top: {y}px"
+        on:mousedown={handleMouseDown}
+        class:z-50={moving}
+>
+    <h1>{text}</h1>
+</div>
+
+
+<svelte:window on:mouseup={handleMouseUp} on:mousemove={handleMouseMove}/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Item from '$lib/components/Item.svelte';
+
+    const itemsize = {
+        width: 50,
+        height: 50
+    }
+
+    type Item = {
+        id: number;
+        x: number;
+        y: number;
+        hover: boolean;
+        text: string;
+    }
+
+    let items: Item[] = [
+        {id: 1, x: 0, y: 0, hover: false, text: 'Item 1'},
+        {id: 2, x: 50, y: 0, hover: false, text: 'Item 2'},
+        {id: 3, x: 10, y: 10, hover: false, text: 'Item 3'},
+    ]
+
+    let hoverId: number = -1;
+
+    function checkForOverlap(currentItem: Item, currentX: number, currentY: number, combine: boolean) {
+        let curIndex = items.findIndex(item => item.id === currentItem.id);
+        const overlapIndex = items.findIndex((item, idx) => {
+            if (idx === curIndex) return false; // Skip checking against itself
+            // Check if the current div overlaps with div
+            const horizontalOverlap = currentX < item.x + itemsize.width && currentX + itemsize.width > item.x;
+            const verticalOverlap = currentY < item.y + itemsize.height && currentY + itemsize.height > item.y;
+            return horizontalOverlap && verticalOverlap;
+        });
+
+        if (combine && overlapIndex !== -1) {
+            console.log(`Item ${curIndex} overlaps with Div ${overlapIndex}`);
+            combineItems(curIndex, overlapIndex);
+        }
+    }
+
+
+    function combineItems(index1: number, index2: number) {
+        let item1 = items[index1];
+        let item2 = items[index2];
+
+        item1.x = (item1.x + item2.x) / 2;
+        item1.y = (item1.y + item2.y) / 2;
+        item1.text = getNewText(item1.text, item2.text);
+        items.splice(index2, 1);
+
+        items = items;
+    }
+
+    function getNewText(text1: string, text2: string) {
+        return text1 + ' ' + text2;
+    }
+</script>
+
+{#each items as item (item.id)}
+    <Item text={item.text} bind:x={item.x} bind:y={item.y}
+          on:drop={(event) => checkForOverlap(item, event.detail.x, event.detail.y, true)}
+    />
+{/each}
